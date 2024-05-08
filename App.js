21bd48d432ffce8d0bf081925573b7c3fec224fe
Lisohub Maksim <maximlisogub.2002@gmail.com>
2024-05-08 21:25:07 +0300

    add fonts

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+
+import Registration from "./src/components/Registration/Registration";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded] = useFonts({
+        "Nunito-Bold": require("./assets/fonts/Nunito/Nunito-Bold.ttf"),
+        "Nunito-Medium": require("./assets/fonts/Nunito/Nunito-Medium.ttf"),
+        "Nunito-Regular": require("./assets/fonts/Nunito/Nunito-Regular.ttf"),
+        "OpenSans-SemiBold": require("./assets/fonts/OpenSans/OpenSans-SemiBold.ttf"),
+    });
+
+    return (
+        <View>
+            <Registration />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: "#fff",
+//         alignItems: "center",
+//         justifyContent: "center",
+//     },
+// });
