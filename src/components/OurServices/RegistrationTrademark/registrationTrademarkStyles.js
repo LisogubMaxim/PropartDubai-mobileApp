2f6df7f09a059589d51d9bae5ea44c84e74673c0
Lisohub Maksim <maximlisogub.2002@gmail.com>
2024-05-14 18:52:45 +0300

    create component RegistrationTrademark

---
+++ src/components/OurServices/RegistrationTrademark/registrationTrademarkStyles.js
@@ -0,0 +1,50 @@
+import { Platform, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    registrationTrademark: {
+        paddingTop: 45,
+    },
+
+    header: {
+        alignItems: "center",
+        marginBottom: 28,
+    },
+
+    headerTitle: {
+        fontFamily: "OpenSans_600SemiBold",
+        fontSize: 20,
+        color: "#333863",
+    },
+
+    body: {
+        marginLeft: 20,
+        marginRight: 20,
+        marginBottom: 21,
+    },
+
+    text: {
+        color: "#6F707A",
+        fontFamily: "Nunito_400Regular",
+        fontSize: 14,
+        marginBottom: 16,
+        //sssssssssssssssssss
+    },
+
+    informationPanel: {
+        marginBottom: 40,
+        gap: 8,
+    },
+
+    documentsRequired: {
+        marginBottom: 40,
+    },
+
+    title: {
+        color: "#000000",
+        fontFamily: "#Nunito_400Regular",
+        fontSize: 16,
+        marginBottom: 16,
+    },
+});
+
+export default styles;
