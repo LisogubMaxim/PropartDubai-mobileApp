891165c850f2cebbc9a905223abafd888630f5ae
Lisohub Maksim <maximlisogub.2002@gmail.com>
2024-06-04 15:04:57 +0300

    added transition to page

--- src/components/areas/AboutArea/AboutArea.jsx
+++ src/components/areas/AboutArea/AboutArea.jsx
@@ -1,4 +1,4 @@
-import { ImageBackground, ScrollView, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
+import { ImageBackground, Modal, ScrollView, StatusBar, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 
 import aboutAreaData from "../../../data/area/aboutAreaData";
 
@@ -8,69 +8,88 @@ import CategorySwitcher from "../../ui/CategorySwitcher/CategorySwitcher";
 import PropertyCard from "../../ui/PropertyCard/PropertyCard";
 import GetConsultation from "../../ui/GetConsultation/GetConsultation";
 import MenuBar from "../../ui/MenuBar/MenuBar";
+import { useState } from "react";
+import AreaProperties from "../AreaProperties/AreaProperties";
 
-const AboutArea = () => {
+const AboutArea = ({ onCloseModal }) => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handlePressMore = () => {
+        setIsModalVisible(true);
+    };
+
+    const closeModal = () => {
+        setIsModalVisible(false);
+    };
+
     return (
         <>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <ImageBackground source={aboutAreaData.image} style={styles.header}>
-                    <TouchableOpacity style={styles.back}>
-                        <BackSvg />
-                    </TouchableOpacity>
-                    <View style={styles.headerText}>
-                        <Text style={styles.title}>{aboutAreaData.title}</Text>
-                        <Text style={styles.price}>{aboutAreaData.price}</Text>
-                    </View>
-                </ImageBackground>
-                <View style={styles.body}>
-                    <View style={styles.description}>
-                        <Text style={styles.titleBody}>Description</Text>
-                        <Text style={styles.text}>{aboutAreaData.description}</Text>
-                    </View>
-                    <View style={{ marginBottom: 40 }}>
-                        <CategorySwitcher data={aboutAreaData.moreInformation} />
-                    </View>
-                    <View style={styles.property}>
-                        <View style={styles.top}>
-                            <Text style={styles.titleBody}>Property in Business Bay</Text>
-                            <TouchableOpacity>
-                                <Text style={styles.more}>More</Text>
-                            </TouchableOpacity>
+            <View style={styles.aboutArea}>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <ImageBackground source={aboutAreaData.image} style={styles.header}>
+                        <TouchableOpacity style={styles.back} onPress={onCloseModal}>
+                            <BackSvg />
+                        </TouchableOpacity>
+                        <View style={styles.headerText}>
+                            <Text style={styles.title}>{aboutAreaData.title}</Text>
+                            <Text style={styles.price}>{aboutAreaData.price}</Text>
+                        </View>
+                    </ImageBackground>
+                    <View style={styles.body}>
+                        <View style={styles.description}>
+                            <Text style={styles.titleBody}>Description</Text>
+                            <Text style={styles.text}>{aboutAreaData.description}</Text>
+                        </View>
+                        <View style={{ marginBottom: 40 }}>
+                            <CategorySwitcher data={aboutAreaData.moreInformation} />
+                        </View>
+                        <View style={styles.property}>
+                            <View style={styles.top}>
+                                <Text style={styles.titleBody}>Property in Business Bay</Text>
+                                <TouchableOpacity onPress={handlePressMore}>
+                                    <Text style={styles.more}>More</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </View>
-                </View>
 
-                <ScrollView
-                    horizontal
-                    contentContainerStyle={{ paddingHorizontal: 20 }}
-                    showsHorizontalScrollIndicator={false}
-                    style={{ marginBottom: 48 }}
-                >
-                    <View style={styles.slider}>
-                        {aboutAreaData.property.map((item, index) => (
-                            <View key={index}>
-                                <PropertyCard
-                                    image={item.image}
-                                    title={item.title}
-                                    width={320}
-                                    height={228}
-                                    amount={item.price}
-                                    isButton={true}
-                                />
-                            </View>
-                        ))}
+                    <ScrollView
+                        horizontal
+                        contentContainerStyle={{ paddingHorizontal: 20 }}
+                        showsHorizontalScrollIndicator={false}
+                        style={{ marginBottom: 48 }}
+                    >
+                        <View style={styles.slider}>
+                            {aboutAreaData.property.map((item, index) => (
+                                <View key={index}>
+                                    <PropertyCard
+                                        image={item.image}
+                                        title={item.title}
+                                        width={320}
+                                        height={228}
+                                        amount={item.price}
+                                        isButton={true}
+                                    />
+                                </View>
+                            ))}
+                        </View>
+                    </ScrollView>
+                    <View style={styles.body}>
+                        <View style={{ marginBottom: 150 }}>
+                            <Text style={styles.titleBody}>Get help from our expert</Text>
+                            <GetConsultation
+                                text={"Our specialist will help you with any question you may have - we are here to help you!"}
+                            />
+                        </View>
                     </View>
                 </ScrollView>
-                <View style={styles.body}>
-                    <View style={{ marginBottom: 150 }}>
-                        <Text style={styles.titleBody}>Get help from our expert</Text>
-                        <GetConsultation
-                            text={"Our specialist will help you with any question you may have - we are here to help you!"}
-                        />
-                    </View>
-                </View>
-            </ScrollView>
-            <MenuBar />
+                <MenuBar />
+            </View>
+
+            <Modal visible={isModalVisible} animationType="slide">
+                <StatusBar backgroundColor="rgba(0,0,0,0.5)" barStyle="light-content" />
+                <AreaProperties onCloseModal={closeModal} />
+            </Modal>
         </>
     );
 };
--- src/components/areas/AreaProperties/areaPropertiesStyles.js
+++ src/components/areas/AreaProperties/areaPropertiesStyles.js
@@ -1,39 +1,52 @@
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  header: {
-    marginHorizontal: 20,
-    paddingBottom: 8,
-  },
+    header: {
+        marginHorizontal: 20,
+        paddingBottom: 8,
+        paddingTop: 60,
+    },
 
-  search: {
-    flexDirection: 'row',
-    gap: 8,
-    marginBottom: 8,
-  },
+    headerTitle: {
+        paddingBottom: 16,
+        flexDirection: "row",
+        alignItems: "center",
+    },
 
-  body: {
-    paddingHorizontal: 20,
-    // marginBottom: 150,
-  },
+    title: {
+        color: "#333863",
+        fontFamily: "OpenSans_600SemiBold",
+        fontSize: 20,
+    },
 
-  property: {
-    gap: 12,
-    marginBottom: 46,
-  },
+    search: {
+        flexDirection: "row",
+        gap: 8,
+        marginBottom: 8,
+    },
 
-  titleBody: {
-    color: '#0F1121',
-    fontFamily: 'OpenSans_600SemiBold',
-    fontSize: 16,
-    marginBottom: 16,
-  },
-  overlay: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
+    body: {
+        paddingHorizontal: 20,
+        // marginBottom: 150,
+    },
+
+    property: {
+        gap: 12,
+        marginBottom: 46,
+    },
+
+    titleBody: {
+        color: "#0F1121",
+        fontFamily: "OpenSans_600SemiBold",
+        fontSize: 16,
+        marginBottom: 16,
+    },
+    overlay: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
 });
 
 export default styles;
